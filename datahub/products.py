55113859414639c05b853b09dc07d75408012b68
Felipe Maza <felipe@felipem.com>
2020-05-07 10:48:46 +0200

    get all products (and filtered)

--- datahub/products.py
+++ datahub/products.py
@@ -3,12 +3,40 @@ import requests
 
 
 class Products(object):
-    url = "https://datahub.ihcantabria.com/v1/public/Products"
+    def __init__(self, url):
+        self.url = url
 
-    def get_all(self):
-        response = requests.get(self.url)
+    def get_all(self, lon_min=None, lat_min=None, lon_max=None, lat_max=None):
+        params = self._set_filters(lon_min, lat_min, lon_max, lat_max)
+
+        if not self._check_there_are_all_coors(params):
+            raise Exception(
+                "lon_min, lon_max, lat_min and lat_max are required for filter"
+            )
+        url_products = f"{self.url}?{'&'.join(params)}"
+
+        response = requests.get(url_products)
         if response.ok:
             data = json.loads(response.content)
         else:
             raise response.raise_for_status()
         return data
+
+    def _set_filters(self, lon_min, lat_min, lon_max, lat_max):
+        params = []
+        if lon_min is not None:
+            params.append(f"xMin={lon_min}")
+        if lat_min is not None:
+            params.append(f"yMin={lat_min}")
+        if lon_max is not None:
+            params.append(f"xMax={lon_max}")
+        if lat_max is not None:
+            params.append(f"yMax={lat_max}")
+        return params
+
+    def _check_there_are_all_coors(self, params):
+
+        n = len(params)
+        if n > 0 and n != 4:
+            return False
+        return True
--- tests/test_products.py
+++ tests/test_products.py
@@ -1,14 +1,31 @@
 import unittest
 from datahub.products import Products
 
+
 class TestProducts(unittest.TestCase):
+    def setUp(self):
+        url = "https://datahub.ihcantabria.com/v1/public/Products"
+
     def test_get_all(self):
         expected_result = 172
-        p = Products()
+        p = Products("https://datahub.ihcantabria.com/v1/public/Products")
         all_products = p.get_all()
         n = len(all_products)
 
         self.assertEqual(n, expected_result)
+
+    def test_get_all_filter(self):
+        expected_result = 121
+        p = Products("https://datahub.ihcantabria.com/v1/public/Products")
+        all_products = p.get_all(lon_min=-10, lon_max=0, lat_min=40, lat_max=50)
+        n = len(all_products)
+
+        self.assertEqual(n, expected_result)
+
+    def test_get_all_filter_partial(self):
+        p = Products("https://datahub.ihcantabria.com/v1/public/Products")
+        with self.assertRaises(Exception):
+            p.get_all(lon_min=-10, lon_max=0, lat_max=50)
 
 
 if __name__ == "__main__":
