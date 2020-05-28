a273bb4a4dc81643e29094f24c0c731683ffc5b8
Felipe Maza <felipe@felipem.com>
2020-05-28 11:09:01 +0200

    configuracion y carga de logger

--- datahub/utils.py
+++ datahub/utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-
+import logging
+import logging.config
+import os
 
 def datetime_to_string(date_obj):
     return date_obj.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -7,3 +9,10 @@ def datetime_to_string(date_obj):
 
 def string_to_datetime(str_date):
     return datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%SZ")
+
+def get_logger(name):
+    config_ini = f"{os.path.dirname(__file__)}/logging.ini"
+    logging.config.fileConfig(config_ini)
+    logger = logging.getLogger(name) 
+    return logger
+
