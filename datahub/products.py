7da5ecf039d79ca3388bd2eeb1ffae7861b1c551
Felipe Maza <felipe@felipem.com>
2020-11-18 10:12:23 +0100

    filter variables by nameShort

--- datahub/products.py
+++ datahub/products.py
@@ -56,19 +56,30 @@ class Products(object):
         logger.info(f"{len(data)} products found")
         return data
 
-    def get_variables(self, product):
+    def get_variables(self, product, var_names=None):
         url_products = "{url}/{id}/Variables".format(
             url=self.url, id=str(product["id"])
         )
 
         response = requests.get(url_products)
         if response.ok:
-            data = json.loads(response.content)
+            result = json.loads(response.content)
+            if var_names: 
+                data = self._filter_variables(result, var_names) 
+            else:
+                data=result
         else:
             logger.error(response.raise_for_status())
             raise response.raise_for_status()
         logger.info(f"{len(data)} variables found")
         return data
+
+    def _filter_variables(self, variables, var_names):
+        variables_ok = []
+        for variable in variables:
+            if variable["nameShort"] in var_names:
+                variables_ok.append(variable)
+        return variables_ok
 
     def _set_filters(self, lon_min, lat_min, lon_max, lat_max):
         params = []
