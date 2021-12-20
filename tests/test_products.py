0c9abf8c1788f43e4c67ba06ef7ab78c96eb4f85
Felipe Maza <felipe@felipem.com>
2021-12-20 09:35:05 +0100

    fix assert in filter test

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,75 +1,81 @@
-import unittest
+import pytest
 from datahub.products import Products
 
 
-class TestProducts(unittest.TestCase):
-    def setUp(self):
-        self.id = 198
-
-    def test_get_product(self):
-        product = Products().get(self.id)
-        self.assertIsNotNone(product)
-
-    def test_get_product_invalid_id(self):
-        id = -5
-        p = Products()
-        product = p.get(id)
-        self.assertIsNone(product)
-
-    def test_get_all(self):
-        p = Products()
-        all_products = p.get_all()
-        n = len(all_products)
-        self.assertGreater(n, 1)
-
-    def test_get_all_filter(self):
-        p = Products()
-        all_products = p.get_all(lon_min=-10, lon_max=0, lat_min=40, lat_max=50)
-        n = len(all_products)
-        self.assertGreater(n, 1)
-
-    def test_get_all_filter_partial(self):
-        p = Products()
-        with self.assertRaises(Exception):
-            p.get_all(lon_min=-10, lon_max=0, lat_max=50)
-
-    def test_get_variables(self):
-        p = Products()
-        product = p.get(self.id)
-        variables = p.get_variables(product)
-        n = len(variables)
-        self.assertEqual(n, 3)
-
-    def test_get_variables_no_product(self):
-        invalid_product = {}
-        p = Products()
-        with self.assertRaises(Exception):
-            p.get_variables(invalid_product)
-
-    def test_filter_name(self):
-        match = Products().filter(name="datahubTests")
-        self.assertEqual(match[0].id, self.id)
-
-    def test_filter_name_partial(self):
-        match = Products().filter(name="datahub")
-        self.assertEqual(match[0].id, self.id)
-
-    def test_filter_name_ko(self):
-        match = Products().filter(name="datahub_ko")
-        self.assertEqual(len(match), 0)
-
-    def test_filter_alias(self):
-        match = Products().filter(alias="Cantabrico")
-        self.assertGreaterEqual(len(match), 1)
-
-    def test_filter_alias_partial(self):
-        match = Products().filter(alias="Cantab")
-        self.assertGreaterEqual(len(match), 1)
-
-    def test_filter_alias_ko(self):
-        match = Products().filter(alias="test_ko")
-        self.assertEqual(len(match), 0)
+def test_get_product():
+    product = Products().get(198)
+    assert product is not None
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_get_product_invalid_id():
+    id = -5
+    p = Products()
+    product = p.get(id)
+    assert product is None
+
+
+def test_get_all():
+    p = Products()
+    all_products = p.get_all()
+    n = len(all_products)
+    assert n > 1
+
+
+def test_get_all_filter():
+    p = Products()
+    all_products = p.get_all(lon_min=-10, lon_max=0, lat_min=40, lat_max=50)
+    n = len(all_products)
+    assert n > 1
+
+
+def test_get_all_filter_partial():
+    p = Products()
+    with pytest.raises(Exception):
+        p.get_all(lon_min=-10, lon_max=0, lat_max=50)
+
+
+def test_get_variables():
+    p = Products()
+    product = p.get(198)
+    variables = p.get_variables(product)
+    n = len(variables)
+    assert n == 3
+
+
+def test_get_variables_no_product():
+    invalid_product = {}
+    p = Products()
+    with pytest.raises(Exception):
+        p.get_variables(invalid_product)
+
+
+def test_filter_name():
+    match = Products().filter(name="datahubTests")
+    assert match[0].id == 198
+
+
+def test_filter_name_partial():
+    match = Products().filter(name="datahub")
+    assert match[0].id == 198
+
+
+def test_filter_name_ko():
+    match = Products().filter(name="datahub_ko")
+    assert len(match) == 0
+
+
+def test_filter_alias():
+    match = Products().filter(alias="Cantabrico")
+
+    assert len(match) > 0
+
+
+def test_filter_alias_partial():
+    match = Products().filter(alias="Cantab")
+    assert len(match) > 1
+
+
+def test_filter_alias_ko():
+    match = Products().filter(alias="test_ko")
+
+    assert len(match) == 0
